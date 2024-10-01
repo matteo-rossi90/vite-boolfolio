9d1296b6de64cc69e70bbaa407712e9d34260ade
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-10-01 21:09:20 +0200

    feat(bonus): get technologies and type from Axios and print them

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -6,23 +6,36 @@ export default {
     name: 'Projects',
     data(){
         return{
-            projects:[]
+            projects:[],
+            types:[],
+            technologies:[],
         }
     },
     methods:{
-        getApi(){
-            axios.get(store.apiUrl + 'projects')
+        getApi(apiUrl, type = 'projects'){
+            axios.get(apiUrl)
             .then(response => {
-                console.log(response.data.data);
-                this.projects = response.data.data;
+                if(type === 'projects'){
+                    console.log(response.data.data);
+                    this.projects = response.data.data;
+                }else{
+                    console.log(type);
+                    this[type] = response.data;
+                }
             })
             .catch(error => {
                 console.error(error); 
             });
+        },
+        getImageUrl(imagePath) {
+            const baseUrl = 'http://127.0.0.1:8000'; 
+            return baseUrl + imagePath;
         }
     },
     mounted(){
-        this.getApi();
+        this.getApi(store.apiUrl + 'projects', 'projects');
+        this.getApi(store.apiUrl + 'type', 'type');
+        this.getApi(store.apiUrl + 'technologies', 'technologies');
     }
 }
 </script>
@@ -35,20 +48,28 @@ export default {
 
         <div class="container-cards">
             <div class="box-cards">
-                <div v-for="project in projects" :key="projects.id" class="cards">
-                    <h4>
+                <div v-for="project in projects" :key="project.id" class="cards">
+                    <h4 class="title">
                         {{ project.title }}
                     </h4>
 
-                    <ul>
+                    <img :src="getImageUrl(project.img)" :alt="project.title">
+
+                    <ul class="list-projects">
                         <li>
-                            <span>Data di inizio: </span><span>{{ project.start_date }}</span>
-                        </li>
-                        <li>
-                            <span>Data di fine:</span><span>{{ project.end_date }}</span>
-                        </li>
-                         <li>
                             <p>{{ project.description }}</p>
+                        </li>
+                        <li>
+                            <span class="badge-type">{{ project.type.name }}</span>
+                        </li>
+                        <li v-if="project.technologies && project.technologies.length > 0">
+                            <ul class="list-techs">
+                                <li v-for="tech in project.technologies" :key="tech.id">
+                                    <small class="badge-tech">
+                                        {{ tech.name }}
+                                    </small>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
                     
@@ -64,24 +85,48 @@ export default {
     .container-cards{
         width: 85%;
         margin: 0 auto;
+        
         .box-cards{
             display: flex;
             flex-wrap:wrap;
             gap: 15px;
             justify-content: start;
-            align-items: center;
-            margin-top: 30px;
+            align-items: stretch;
+            margin: 30px 0;
 
             .cards{
-                height: 300px;
-                padding: 10px;
+                // height: 500px;
+                padding: 20px;
                 width: calc(100% / 3 - 10px);
                 border-radius: 10px;
                 background-color: rgb(226, 226, 226);
-                ul,
-                li{
-                    margin: 20px 0;
+                
+                img{
+                    margin: 15px 0;
+                    object-fit:cover;
+                    width: 100%;
+                    aspect-ratio: 12 / 9;
+                    border-radius: 10px;
                 }
+
+                ul, li {
+                    margin: 20px 0;
+                    padding: 0;
+                }
+
+
+                .list-techs{
+                    display: flex;
+                    flex-wrap:wrap;
+                    gap: 10px;
+                    justify-content: start;
+                
+
+
+                }
+
+                
+
             }
         }
     }
